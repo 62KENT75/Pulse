01048b3110db37136182e9cb41807531cb9de7e1
Sergey_Gershun <gershun62@gmail.com>
2023-12-16 11:24:26 +0200

    pageup

--- src/js/script.js
+++ src/js/script.js
@@ -79,4 +79,41 @@ $(document).ready(function(){
   valideForms('#order form');
 
   $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#up']").click(function() {
+    const _href = $(this).attr("href");
+    $("html, bode").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
 });
